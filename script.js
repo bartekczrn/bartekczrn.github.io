ab888c6ffc92b05656893fb70979065691edbc9e
Bartosz Czernicki <bartekczernicki719@gmail.com>
2023-03-17 15:06:24 +0100

    First update

---
+++ script.js
@@ -0,0 +1,14 @@
+function NewTabInf() {
+    // onclick="location.href='https://informatyk.edu.pl/kwalifikacja-inf-02/';"
+    window.open("https://informatyk.edu.pl/kwalifikacja-inf-02/", "_blank");
+}
+
+function NewTabGoogle() {
+    // onclick="location.href='https://informatyk.edu.pl/kwalifikacja-inf-02/';"
+    window.open("https://www.coursera.org/account/accomplishments/specialization/certificate/SJV5HYQ92PKW", "_blank");
+}
+
+function NewTabMeta() {
+    // onclick="location.href='https://informatyk.edu.pl/kwalifikacja-inf-02/';"
+    window.open("https://www.coursera.org/account/accomplishments/specialization/certificate/K7A3DJJWQGLK", "_blank");
+}
